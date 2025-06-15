7ca3da285bc27b0089a6442e9c0183b066984b19
Asilbek Khujanazarov <asilbekpersonal@gmail.com>
2025-06-15 12:50:57 +0500

    Initial commit

--- obj/Release/net9.0/Autotest.Platform.AssemblyInfo.cs
+++ obj/Release/net9.0/Autotest.Platform.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Autotest.Platform")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+211e95b2a63cef966571035a6cbb02347b2c9b7d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1419e85b6d9b6fcc5536d995ccdf779bcc6ee18")]
 [assembly: System.Reflection.AssemblyProductAttribute("Autotest.Platform")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Autotest.Platform")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/wwwroot/angular-app-manifest.mjs
+++ src/wwwroot/angular-app-manifest.mjs
@@ -7,8 +7,8 @@ export default {
   routes: undefined,
   entryPointToBrowserMapping: {},
   assets: {
-    'index.csr.html': {size: 9175, hash: '0ddf2f44954d614515642bf63edfa2b1759ec234c310982a0a2339f2a1212605', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1109, hash: '128f473cd7f9055c718b4eb7d122a9629c0a58d8b9c23f87d14ab885fefd69e7', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 9175, hash: '5845ee57b32319420051785cc7e4bbb890273d24c2a334c90f5f8c5440b6ecdb', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1109, hash: '28a55dfbbe79f8bc21e3d6030b0e8439f5d8d0a63e81f3f0ac366a89a402e91d', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-JVNHWSQE.css': {size: 8755, hash: '6OQThRAm/hU', text: () => import('./assets-chunks/styles-JVNHWSQE_css.mjs').then(m => m.default)}
   },
 };
