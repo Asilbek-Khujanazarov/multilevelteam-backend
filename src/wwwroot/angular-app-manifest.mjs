905fc301865b7a4ec8bac4326e30c64fc4766f7b
Asilbek Khujanazarov <asilbekpersonal@gmail.com>
2025-06-15 11:31:47 +0500

    domen changed3

--- src/wwwroot/angular-app-manifest.mjs
+++ src/wwwroot/angular-app-manifest.mjs
@@ -7,8 +7,8 @@ export default {
   routes: undefined,
   entryPointToBrowserMapping: {},
   assets: {
-    'index.csr.html': {size: 9175, hash: '9c451f2c609ddbbd15c99eb9b8c110475c6cb3cf2de5ca4079df96d863d1e437', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1109, hash: '1ae0ce18a77669f12fff0c96ef1ee7b7410fa762d693542df9bb8de6b3115a71', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 9175, hash: '0ddf2f44954d614515642bf63edfa2b1759ec234c310982a0a2339f2a1212605', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1109, hash: '128f473cd7f9055c718b4eb7d122a9629c0a58d8b9c23f87d14ab885fefd69e7', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-JVNHWSQE.css': {size: 8755, hash: '6OQThRAm/hU', text: () => import('./assets-chunks/styles-JVNHWSQE_css.mjs').then(m => m.default)}
   },
 };
