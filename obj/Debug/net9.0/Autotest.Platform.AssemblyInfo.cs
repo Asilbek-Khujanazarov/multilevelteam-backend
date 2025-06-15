93168d5e49d3dcb9fea88c966cb86e8657395ee1
Asilbek Khujanazarov <asilbekpersonal@gmail.com>
2025-06-15 13:37:37 +0500

    wwwroot fayllarini qo‘shish

--- obj/Debug/net9.0/Autotest.Platform.AssemblyInfo.cs
+++ obj/Debug/net9.0/Autotest.Platform.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Autotest.Platform")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c6e4b66443e8618fd44f8a4ad5f1475d19934a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14390a5bb2d7650afef52e909aafefbc0088d31c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Autotest.Platform")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Autotest.Platform")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
